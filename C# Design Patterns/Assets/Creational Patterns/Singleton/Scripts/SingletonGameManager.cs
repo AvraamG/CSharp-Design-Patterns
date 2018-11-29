cee670563d2edb6b0e0a11c8cc72a4fb388a6bbb
AbrahamGeorgiadis <georgiadis.ab@gmail.com>
2018-11-29 15:27:35 +0100

    Finished the Singleton example

--- C# Design Patterns/Assets/Creational Patterns/Singleton/Scripts/SingletonGameManager.cs
+++ C# Design Patterns/Assets/Creational Patterns/Singleton/Scripts/SingletonGameManager.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingletonGameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Singleton.Example
+{
+    public class SingletonGameManager : MonoBehaviour
+    {
+        private static SingletonGameManager _instance;
+        public static SingletonGameManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        /// <summary>
+        /// if the private static variable of the instance is not assigned, assign it to this script. Otherwise take care of it, destroy/deactivate etc.
+        /// </summary>
+        private void InitializeSingleton()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                Debug.LogFormat("Initialized Singleton to {0}", this.gameObject.name);
+            }
+            else
+            {
+                Debug.LogErrorFormat("More than 1 Singleton GameManagers in the scene destroyed the duplicate {0}", this.gameObject.name);
+                Destroy(this.gameObject);
+            }
+        }
+        /// <summary>
+        /// Hello world public method so other classes can access it through the public static SingletonGameManager.Instance.
+        /// </summary>
+        public void SayHello()
+        {
+            Debug.LogFormat("Hello from {0}", this.gameObject.name);
+        }
+
+
+    }
 }
+
